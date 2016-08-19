733ea762c404e3ac31b34048b6088cb07e9c588a
jarped <jarle.pedersen@norconsult.com>
2016-08-19 16:34:25 +0200

    reads layers and their structure from old config of norgeskart

---
+++ parseAnsikter.py
@@ -0,0 +1,70 @@
+import json
+import yaml
+import urllib
+import pprint
+
+def GetYaml(url):
+  stream=urllib.urlopen(url).read()
+  try:
+    if '---' in stream:
+      return yaml.load(stream.split('---')[1])
+    return yaml.load(stream)
+  except yaml.YAMLError as exc:
+    print(exc)
+
+def GetYamlNodes(yaml, name):
+  nodes = []
+  for node in yaml:
+    nodes.append(node[name])
+  return nodes
+
+def ReadLayers(layers):
+  i=0
+  for layer in layers:
+    layerUrl=base + branch + '/themes/' + layer + '.yaml'
+    layerYaml=GetYaml(layerUrl)
+    layerGroup=layerYaml["layerGroup"]
+    if layerGroup not in ansikter[ansikt]["layers"].keys():
+      ansikter[ansikt]["layers"][layerYaml["layerGroup"]]={}
+    ansikter[ansikt]["layers"][layerYaml["layerGroup"]][i]=layerYaml
+    i+=1
+    #ansikter[ansikt]["layers"][i]["layername"]=layer.split('/')[-1]
+    continue
+    if "url" in layerYaml.keys():
+      print '\t\t\t' + layerYaml["url"]
+    if "layer" in layerYaml.keys():
+      print '\t\t\t' + layerYaml["layer"]
+    elif "layers" in layerYaml.keys():
+      print '\t\t\t' + layerYaml["layers"]
+    else:
+      print '\t\t\tNo layers found.'
+      print layerYaml
+
+
+json_data=open('ansikter.json').read()
+data = json.loads(json_data)
+ansikter = {}
+
+
+for ansikt in data["ansikter"]:
+  print "Ansikt: " + ansikt
+
+  ansikter[ansikt] = {}
+
+  branch=data["ansikter"][ansikt]
+  base=data["urls"]["rawbase"]
+  ansikter[ansikt]["index"]=base + branch + data["urls"]["index"]
+
+  indexYaml=GetYaml(ansikter[ansikt]["index"])
+
+  ansikter[ansikt]["baselayers"]=GetYamlNodes(indexYaml["baselayers"], "template")
+  overlays=GetYamlNodes(indexYaml["overlays"], "include")
+  ansikter[ansikt]["layers"] = {}
+
+  ReadLayers(overlays)
+  break
+ 
+pprint.pprint(ansikter)
+
+#  secondary=base + branch + data["urls"]["secondary"]
+#  Get(secondary)
