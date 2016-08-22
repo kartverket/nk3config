ea911b43ea1cbea07011101bece4ee6b8d271b1f
jarped <jarle.pedersen@norconsult.com>
2016-08-22 17:58:32 +0200

    added wms

--- convert2geoinnsyn.py
+++ convert2geoinnsyn.py
@@ -3,6 +3,7 @@ import json
 import xml.etree.ElementTree as ET
 import project
 import maplayer
+import wms
 
 def LoadFile(fileName):
   return file.open(fileName)
@@ -15,22 +16,39 @@ for ansikt in data:
   projectConfig=project.Project()
   projectConfig.sidetitle=ansikt
   projectConfig.headertitle=ansikt
-  mapgroups=0
+  groupid=0
   baselayer=data[ansikt]["baselayers"]
   for group in data[ansikt]["groups"]:
     maplayerConfig=maplayer.Maplayer()
-    maplayerConfig.groupid=mapgroups
-    maplayerConfig.index=mapgroups
+    maplayerConfig.groupid=groupid
+    maplayerConfig.index=groupid
     maplayerConfig.name=group
     projectConfig.maplayer.append(maplayerConfig)
     print '\t' + group
-    for layer in data[ansikt]["groups"][group]:
-      print '\t\t' + data[ansikt]["groups"][group][layer]["name"]
-      if(data[ansikt]["groups"][group][layer]["template"] == 'layers/wms'):
+    for layerName in data[ansikt]["groups"][group]:
+      layer=data[ansikt]["groups"][group][layerName]
+      print '\t\t' + layer["name"]
+      if(layer["template"] == 'layers/wms'):
         print '\t\tWMS'
-      elif(data[ansikt]["groups"][group][layer]["template"] == 'layers/wmts'):
-        print '\t\tWMTS'
-      else:
-        print '\t\tVector'
+        wmsConfig=wms.Wms()
+        wmsConfig.type="overlay"
+        wmsConfig.groupid=groupid
+        wmsConfig.url=layer["url"] + " " + layer["layers"]
+        wmsConfig.opencacheurl="Map/GetMap?" + layer["url"] + " " + layer["layers"]
+        wmsConfig.grouptitle=group
+        wmsConfig.name=layer["name"]
+        wmsConfig.params["Layers"]=layer["layers"]
+        wmsConfig.Layers["Layer"]["title"]=layer["layers"]
+        wmsConfig.Layers["Layer"]["name"]=layer["layers"]
+        if ("getfeature" in layer.keys()):
+          print "\t\tgetfeature: " + str(layer["getfeature"])
+          wmsConfig.Layers["Layer"]["queryable"]=layer["getfeature"]
+        print wmsConfig.GetXml().text
 
-    mapgroups+=1
+      elif(layer["template"] == 'layers/wmts'):
+        print '\t\tWMTS'
+
+      else:
+        print '\t\t' + layer["template"]
+
+    groupid+=1
